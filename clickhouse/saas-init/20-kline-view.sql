b04345a9dc4774a5bcac8d251a2ae2eb3781842a
deshuai.kong <deshuai.kong@sumscope.com>
2026-08-27 09:01:25 +0800

    Use direct ZooKeeper port and fix kline view init

--- clickhouse/saas-init/20-kline-view.sql
+++ clickhouse/saas-init/20-kline-view.sql
@@ -2,22 +2,40 @@ CREATE VIEW IF NOT EXISTS dc.kline_view AS
 SELECT
     location,
     startTime,
-    argMax(endTime, createTime) AS endTime,
+    endTime,
     securityID,
-    upperUTF8(text) AS text,
+    text,
     fmtTime,
-    argMax(inf1, createTime) AS inf1,
-    argMax(open, createTime) AS open,
-    argMax(close, createTime) AS close,
-    argMax(low, createTime) AS low,
-    argMax(high, createTime) AS high,
-    argMax(turnover, createTime) AS turnover,
-    argMax(volume, createTime) AS volume,
-    max(createTime) AS createTime
-FROM dc.kline
-GROUP BY
-    location,
-    startTime,
-    securityID,
-    upperUTF8(text),
-    fmtTime;
+    inf1,
+    open,
+    close,
+    low,
+    high,
+    turnover,
+    volume,
+    latestCreateTime AS createTime
+FROM
+(
+    SELECT
+        location,
+        startTime,
+        argMax(endTime, createTime) AS endTime,
+        securityID,
+        upperUTF8(text) AS text,
+        fmtTime,
+        argMax(inf1, createTime) AS inf1,
+        argMax(open, createTime) AS open,
+        argMax(close, createTime) AS close,
+        argMax(low, createTime) AS low,
+        argMax(high, createTime) AS high,
+        argMax(turnover, createTime) AS turnover,
+        argMax(volume, createTime) AS volume,
+        max(createTime) AS latestCreateTime
+    FROM dc.kline
+    GROUP BY
+        location,
+        startTime,
+        securityID,
+        upperUTF8(text),
+        fmtTime
+);
